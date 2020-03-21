ca8d7419bb47c19b96d25f39f275f12208b38ad0
Jie Chen <jiechencn@qq.com>
2020-03-21 20:39:37 +0800

    finished YieldTest with IEnumerable interface

---
+++ YieldTest/Class.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YieldTest
+{
+    class Class: IEnumerable<Student>
+    {
+        private LinkedList<Student> students = new LinkedList<Student>();
+        public Class()
+        {
+
+        }
+        public void Add(Student s)
+        {
+            students.AddLast(s);
+        }
+
+        public IEnumerator<Student> GetEnumerator()
+        {
+            foreach(Student s in students)
+            {
+                if (s.ID %2==0)
+                    yield return s;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ YieldTest/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace YieldTest
+{
+    internal class Student
+    {
+        internal string Name { get;}
+        internal int ID { get; }
+        internal Student(string name, int id)
+        {
+            Name = name;
+            ID = id;
+        }
+
+
+    }
+}
